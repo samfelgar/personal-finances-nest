f0457b0488b2b7ca7cf3521569fe6977062e28cb
Samuel Felipe <samfelgar@gmail.com>
2022-10-07 03:41:14 -0300

    Protect user's routes

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -6,15 +6,18 @@ import {
   Param,
   Patch,
   Post,
+  UseGuards,
   UseInterceptors,
 } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { TypeORMError } from 'typeorm';
+import { JwtGuard } from '../auth/guard/jwt.guard';
 
 @Controller('users')
 @UseInterceptors(ClassSerializerInterceptor)
+@UseGuards(JwtGuard)
 export class UsersController {
   constructor(private usersService: UsersService) {}
 
